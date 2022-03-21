9a6b162f38bab2f12ad241c2d22bbea890b97a63
TomCevasco <tomascevasco@gmail.com>
2022-03-21 16:10:15 -0300

    Se agrega HTML y CSS de Seccion Registro y Login + App.js y package.json

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { link } = require("fs");
+
+const app = express()
+
+app.use(express.static("public"));
+const path = require("path")
+
+
+
+app.listen(3004, () => {
+
+    console.log("el servidor esta corriendo en puerto 3004");
+})
+
+
+
+        
+    app.get("/Login",(req,res) =>{
+
+        res.sendFile(path.resolve(__dirname, "./Views/Login.html"))
+        
+        }) ;
+
+
+        app.get("/Registro",(req,res) =>{
+
+            res.sendFile(path.resolve(__dirname, "./Views/Registro.html"))
+            
+            }) ;
